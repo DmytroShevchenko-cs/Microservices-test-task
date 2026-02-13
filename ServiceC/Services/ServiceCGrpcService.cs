03056629cd9e4bcd879037e65547864a380dcf42
Dmytro <dima.shevchenko10@gmail.com>
2026-02-13 13:12:36 +0200

    feat(docker): add docker compose

--- ServiceC/Services/ServiceCGrpcService.cs
+++ ServiceC/Services/ServiceCGrpcService.cs
@@ -3,14 +3,12 @@ namespace ServiceC.Services;
 using Grpc.Core;
 using Shared.Protos;
 
-public class ServiceCGrpcService : ServiceC.ServiceCBase
+public class ServiceCGrpcService(ILogger<ServiceCGrpcService> logger) : ServiceC.ServiceCBase
 {
     public override async Task<FinalizeResponse> FinalizeTransaction(
         FinalizeRequest request,
         ServerCallContext context)
     {
-        await Task.Delay(500);
-        
         var overallSuccess = request.ServiceA && request.ServiceB;
 
         var response = new FinalizeResponse
@@ -19,6 +17,8 @@ public class ServiceCGrpcService : ServiceC.ServiceCBase
             ProcessedAt = DateTime.UtcNow.ToString("O")
         };
 
+        logger.LogInformation("{RequestTransactionId} finished", request.TransactionId);
+        
         return response;
     }
 }
